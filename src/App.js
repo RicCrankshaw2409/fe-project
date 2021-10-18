526ca0823202890f25fc12253c41fddd80c29bd7
Richard Crankshaw <ric_24@hotmail.co.uk>
2021-10-18 18:10:28 +0100

    Borwser router set-up in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <SignInPage />
+          </Route>
+          <Route exact path="/reviews">
+            <NavBar />
+            <CategoryBar />
+            <ReviewsPage />
+          </Route>
+          <Route exact path="/reviews/:review_id">
+            <NavBar />
+            <CategoryBar />
+            <CommentsPage />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
